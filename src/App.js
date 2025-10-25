ffdff5cb41b7ed5a76a2868830e81a9c054ff2f9
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-10-26 03:24:07 +0900

    feat : 테스팅 코드 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  return (
+    <div className="contanier">
+      <section className="count_section">
+        <h1 data-testid="counter">{count}</h1>
+      </section>
+      <section>
+        <button
+          disabled={disabled}
+          data-testid="plus-button"
+          onClick={() => setCount(count + 1)}
+          style={{ marginRight: '20px' }}
+        >
+          +
+        </button>
+        <button
+          disabled={disabled}
+          data-testid="minus-button"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '5px',
+          }}
+        >
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: 'blue' }}
+            onClick={() => setDisabled(!disabled)}
+          >
+            on/off
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
